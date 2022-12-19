6c6ca6c8e62ec69806472d768758862d311006a6
hadisptr <100454821+hadisptr@users.noreply.github.com>
2022-12-19 17:28:27 +0700

    Add files via upload

---
+++ src/main/java/Game/PongGame.java
@@ -0,0 +1,12 @@
+package Game;
+
+import Game.GameFrame;
+
+public class PongGame {
+
+	public static void main(String[] args) {
+		
+		GameFrame frame = new GameFrame();
+		
+	}
+}
---
+++ src/main/java/Game/Score.java
@@ -0,0 +1,33 @@
+package Game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import Game.Score;
+
+public class Score extends Rectangle{
+
+	static int GAME_WIDTH;
+	static int GAME_HEIGHT;
+	int player1;
+	int player2;
+	
+	Score(int GAME_WIDTH, int GAME_HEIGHT){
+		Score.GAME_WIDTH = GAME_WIDTH;
+		Score.GAME_HEIGHT = GAME_HEIGHT;
+	}
+	public void draw(Graphics g) {
+		g.setColor(Color.PINK);
+		g.setFont(new Font("Arial",Font.PLAIN,60));
+		
+		g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+		g.drawOval(GAME_WIDTH/-2, 0, GAME_WIDTH, GAME_HEIGHT);
+		g.drawOval(GAME_WIDTH/2, 0, GAME_WIDTH, GAME_HEIGHT);
+		//g.drawOval(GAME_HEIGHT/2, GAME_WIDTH/2, GAME_WIDTH, GAME_HEIGHT);
+		
+		g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10), (GAME_WIDTH/2)-85, 50);
+		g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10), (GAME_WIDTH/2)+20, 50);
+	}
+}
